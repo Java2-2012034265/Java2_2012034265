5d63e830b84bdcd18b8111fb7745c87409546114
inhye <c1105047@naver.com>
2016-04-07 20:32:18 +0900

    4월7일

---
+++ src/_0407/Method.java
@@ -0,0 +1,13 @@
+package _0407;
+
+public class Method {
+	
+	void met(int x){
+		System.out.println("int"+x);
+		
+	}
+
+	void met(double y){
+		System.out.println("double"+y);
+	}
+}
---
+++ src/_0407/Television.java
@@ -0,0 +1,7 @@
+package _0407;
+
+public class Television {
+	int channel;
+	int volume;
+	boolean onOff;
+}
