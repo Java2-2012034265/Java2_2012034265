df07d2d61214efccb518e076b65424b12ea07fb1
inhye <c1105047@naver.com>
2016-05-19 20:27:55 +0900

    5월 19일

--- src/sample0513/Rectangle.java
+++ src/sample0513/Rectangle.java
@@ -2,9 +2,9 @@ package sample0513;
 
 public class Rectangle extends shape {
 
-	pirvate int x1, y1;
+	private int x1, y1;
 	
-	public rectangle() {x1=5,y1=5}
+	public rectangle() {x1 =5 , y1 = 5}
 	public void move (int_x,int_y) {x=x1+_x; y=y1+_y;}
 	
 	public void info() { System.out.println("R x : "+x+ y: "+y");
